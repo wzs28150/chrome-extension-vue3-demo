41737e9a1f6119f2b2af2d88f951718b63fa0b3d
wzs28150 <wz666s@163.com>
2024-08-04 22:45:12 +0800

    test devtools

--- src/devtools/index.js
+++ src/devtools/index.js
@@ -1,5 +1,8 @@
-chrome.devtools.panels.create('devtools', null, '/src/devtools.html')
-console.log(window.chrome.devtools)
-if (window.chrome.devtools.panels.themeName === 'dark') {
+chrome.devtools.panels.create('devtools', null, '/src/devtools.html', () => {
+  console.log('user switched to this panel')
+})
+console.log(chrome.devtools.panels)
+const theme = chrome.devtools.panels.themeName
+if (theme.value === 'dark') {
   document.documentElement.classList.add('dark')
 }
